d1dcd6b0dbf9595f21308b2a6116f7e74538884f
amrmhassan <amrmhassanmarsafa@gmail.com>
2021-01-13 23:54:57 +0200

    make it better

--- db.js
+++ db.js
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 export default {
   connectDB: () => {
     mongoose
-      .connect(process.env.db_link, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      })
+      .connect(
+        process.env.db_link.replace('<PASSWORD>', process.env.db_Password),
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+          useCreateIndex: true,
+        }
+      )
       .then(() => console.log('connected to db'))
       .catch((err) => console.log(err.message));
   },
